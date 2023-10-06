14434f2395eb81cffd12f7c51ee623b5b35798e8
VietHungHoang <viethunghoang1508@gmail.com>
2023-10-07 00:43:21 +0700

    Việt Hưng Hoàng

---
+++ test.py
@@ -0,0 +1,14 @@
+global_variable = 10  # Đây là biến toàn cục
+
+def function1():
+    global global_variable  # Sử dụng từ khóa global để tham chiếu đến biến toàn cục
+    local_variable = 5  # Đây là biến cục bộ trong hàm function1
+    global_variable += 1  # Thay đổi giá trị của biến toàn cục
+    print(global_variable)  # Có thể truy cập và in giá trị của biến toàn cục từ trong hàm function1
+
+def function2():
+    # Đây là hàm khác và không thể truy cập biến cục bộ của hàm function1
+    print(global_variable)  # Có thể truy cập và in giá trị của biến toàn cục từ trong hàm function2
+
+function1()
+function2()
